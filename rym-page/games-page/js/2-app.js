01bbf2ba70f902f41a389fa710a0643d4dc748db
Don Santis <santisamaya@gmail.com>
2023-01-22 04:59:02 -0600

    update sabadeño

--- rym-page/games-page/js/2-app.js
+++ rym-page/games-page/js/2-app.js
@@ -33,20 +33,27 @@ function createCard(data) {
 
   data.results.forEach((item) => {
     // console.log(item.status);
+    const loca = item.location.name;
     const clone = templateCard.cloneNode(true);
 
 
     if (item.status == 'Alive') {
-         clone.querySelector(".card-status-char").className = 'card-status-char rounded-circle';
+         clone.querySelector(".card-status-char").className = 'card-status-char ';
+         clone.querySelector(".card-status-char").textContent = 'Live';
      }
      else{
-        clone.querySelector(".card-status-char").className = 'card-status-char2 rounded-circle';
-
+        clone.querySelector(".card-status-char").className = 'card-status-char card-status-char2  ';
+        clone.querySelector(".card-status-char").textContent = 'Dead';
      }
      
-    clone.querySelector(".card-title-char").textContent = item.name;
+    clone.querySelector(".card-title-char").textContent = item.name.substring(0, 15);
     clone.querySelector(".card-img-top").setAttribute("src", item.image);
-    
+
+
+    clone.querySelector(".specie-description").textContent = item.species;
+    clone.querySelector(".gender-description").textContent = item.gender;
+    clone.querySelector(".ubication-description").textContent = item.location.name.substring(0, 17);
+    clone.querySelector(".location-description").textContent = item.origin.name.substring(0, 18);
     // guardamos en el fragment para evitar el reflow
     fragment.appendChild(clone);
   });
@@ -83,10 +90,9 @@ prevButton.addEventListener("click", () => {
 
 
 function bigImg(x) {
-  console.log(x.style.height);
-  x.className = 'card card-char4 absolute m-4';
+  x.className = 'card card-char-base card-char4 absolute m-4';
 }
 
 function normalImg(x) {
-  x.className = 'card card-char3 absolute m-4';
+  x.className = 'card card-char-base card-char3 absolute m-4';
 }
