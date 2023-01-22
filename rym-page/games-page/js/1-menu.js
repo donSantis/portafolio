637c1ebf34ec491e8aecda5c88ecb62ece169170
Don Santis <santisamaya@gmail.com>
2023-01-22 14:39:59 -0600

    update mid domingo

--- rym-page/games-page/js/1-menu.js
+++ rym-page/games-page/js/1-menu.js
@@ -1,11 +1,1 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "30px";
-    document.querySelector(".separator-top").style.top = "20px";
-  } else {
-    document.getElementById("navbar").style.top = "-555px";
-    document.querySelector(".separator-top").style.top = "-100px";
-  }
-}
+ 
--- rym-page/games-page/js/2-app.js
+++ rym-page/games-page/js/2-app.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-  getAll();
+  getAll2();
 });
-
-
+ 
 function getAll() {
   function getAllId(id) {
     $.ajax({
@@ -11,7 +10,7 @@ function getAll() {
       dataType: 'json',
       data: { request: 'mostrarHorarios' },
       success: function (data) {//success
-       // console.log(data);
+        // console.log(data);
         createCard(data);
         // muestraSlides(data.results.length);
       }, fail: function (data) {
@@ -24,7 +23,26 @@ function getAll() {
   }
 
 }
+function getAll2() {
 
+  $.ajax({
+    type: "get",
+    url: 'https://rickandmortyapi.com/api/character',
+    dataType: 'json',
+    data: { request: 'mostrarHorarios' },
+    success: function (data) {//success
+      // console.log(data);
+      createCard(data);
+      // muestraSlides(data.results.length);
+    }, fail: function (data) {
+      console.log("fail all city");
+    },
+  });
+  //  for (let i = 1; i < 21; i++) {
+  //  getAllId(i);
+  //}
+
+}
 function createCard(data) {
   var position = 0;
   var cardSide = document.querySelector("#slides-container");
@@ -38,14 +56,14 @@ function createCard(data) {
 
 
     if (item.status == 'Alive') {
-         clone.querySelector(".card-status-char").className = 'card-status-char ';
-         clone.querySelector(".card-status-char").textContent = 'Live';
-     }
-     else{
-        clone.querySelector(".card-status-char").className = 'card-status-char card-status-char2  ';
-        clone.querySelector(".card-status-char").textContent = 'Dead';
-     }
-     
+      clone.querySelector(".card-status-char").className = 'card-status-char';
+      clone.querySelector(".card-status-char2").className = 'd-none';
+    }
+    else {
+      clone.querySelector(".card-status-char2").className = 'card-status-char card-status-char2  ';
+      clone.querySelector(".card-status-char").className = 'd-none';
+    }
+
     clone.querySelector(".card-title-char").textContent = item.name.substring(0, 15);
     clone.querySelector(".card-img-top").setAttribute("src", item.image);
 
@@ -95,4 +113,74 @@ function bigImg(x) {
 
 function normalImg(x) {
   x.className = 'card card-char-base card-char3 absolute m-4';
+}
+
+
+
+
+function myFunction3() {
+  var input, filter, ul, li, a, i, txtValue;
+
+  input = document.getElementById("myInput");
+  //console.log(input.value);
+  filter = input.value.toUpperCase();
+
+  ul = document.getElementById("slides-container");
+  li = ul.getElementsByClassName("slide");
+
+  for (i = 0; i < li.length; i++) {
+
+      //console.log(li.length);
+      a = li[i].getElementsByTagName("a");
+      console.log(a);
+
+
+      txtValue = a.textContent;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+
+  }
+}
+function myFunction() {
+  let input = document.getElementById('myInput').value;
+   
+  input=input.toLowerCase();
+  //console.log(input);
+  let y = document.getElementsByClassName('slide');
+  let x = document.getElementsByClassName('card-char-base');
+  console.log(x);
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+
+        y[i].style.display="list-item";
+        x[i].style.display="list-item";
+      }
+      else {
+      
+        y[i].style.display="list-item";
+        x[i].style.display="list-item";
+      }
+  }
+
+  
+}
+function myFunction2() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      console.log(a);
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
 }
